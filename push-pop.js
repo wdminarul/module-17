c7886fe52760db10a8deb7d1b2da3bd637f4d20f
wdminarul <wdminarul100@gmail.com>
2024-07-27 02:17:25 +0600

    push-pop

---
+++ push-pop.js
@@ -0,0 +1,25 @@
+var number = [11, 44,35, 45, 56, 67, 57 ]
+//set an ellement in array of last position
+number.push(88);
+number.push(90)
+console.log(number)
+
+//pop or delete an ellemnt from array of last value
+var number2 = [57,798, 897,3245, 44,65 , 5242, 234,]
+var lossElement = number2.pop()
+console.log(lossElement)
+console.log(number2)
+
+// show loss Element of pop 
+
+// add element from begining of array 
+var number3 = [67, 85, 54, 26, 78,];
+number3.unshift(12);
+console.log(number3);
+
+// remove element from begining of array 
+
+var number4 = [56, 37, 47, 27, 58, 90, 24];
+number4.shift()
+console.log(number4)
+
