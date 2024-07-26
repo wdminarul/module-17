6f6050d71639ce604df388b94ee9c8797a855431
wdminarul <wdminarul100@gmail.com>
2024-07-27 02:31:27 +0600

    conparision

---
+++ conpasision.js
@@ -0,0 +1,15 @@
+var number1 = 55;
+var number2 = 67;
+
+console.log(number1 < number2) //number2 er theke 1 choto kina
+console.log(number1 > number2) //number1 er theke 2 boro kina
+console.log(number1 <= number2) //number2 er theke 1 choto othoba soman kina
+console.log(number1 >= number2) //number1 er theke 2 choto othoba soman kina
+console.log(number1 == number2) //number2 and 1 soman kina
+console.log(number1 != number2) //number2 er theke 1 soman noy kina
+
+
+// multiple conditions apply 
+
+money1> money2 && height1 > height2 && result1 > result2 //sob gula condition milate hobe
+money1> money2 || height1 > height2 || result1 > result2 //jekono ekta condition milate hobe
