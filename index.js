d0be6f0f7ac46bcf2e8f6887d573fdd3061f942c
wdminarul <wdminarul100@gmail.com>
2024-07-27 02:01:39 +0600

    array

---
+++ index.js
@@ -0,0 +1,14 @@
+
+// javascript Array 
+var friends = ['habiba', 'shimul', 'joy', 'shadhin', 'Yakin'];
+var age = [23, 24, 20, 21, 25, 40];
+ console.log(friends)
+//  length of value 
+ console.log(age.length)
+ //position on index in array
+ console.log(friends.indexOf('joy'))
+// find value from array 
+ console.log(friends[3])
+// include value from same position use index 
+ age[3] = 55;
+ console.log(age)
