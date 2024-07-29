d058924774fc4c621be0c44f7bff283740c63632
wdminarul <wdminarul100@gmail.com>
2024-07-29 22:54:01 +0600

    consition-final-commit

--- condition.js
+++ condition.js
@@ -1,14 +1,22 @@
 var sllary = 50000;
-var isGratuated = false;
+var isGratuated = true;
 var salaryIncriment = true;
 var yearlyBonus = 50000;
 var perfermanceBonus = false;
 var promotion = true;
+//  same time multiple consition in one line 
+// if ((sllary>=50000 && isGratuated === true && yearlyBonus >30000)|| perfermanceBonus == true && promotion == true ){
+//  console.log('Yes I can Do this job in-sha-Allah')
+// }
+// else{
+//     console.log("Sorry! No I can't do that job")
+// }
 
+// multiple condition and neestes condition 
 
-if ((sllary>=50000 && isGratuated === true && yearlyBonus >30000)|| perfermanceBonus == true && promotion == true ){
- console.log('Yes I can Do this job in-sha-Allah')
+if(sllary >= 50000){
+    console.log('Ami vebe dekhbo')
 }
-else{
-    console.log("Sorry! No I can't do that job")
+else if( isGratuated == true){
+    console.log()
 }
