d2e34f8a94e35327f4e87bf5cc179e8b33feb9cc
wdminarul <wdminarul100@gmail.com>
2024-07-29 22:53:24 +0600

    condition

---
+++ condition.js
@@ -0,0 +1,14 @@
+var sllary = 50000;
+var isGratuated = false;
+var salaryIncriment = true;
+var yearlyBonus = 50000;
+var perfermanceBonus = false;
+var promotion = true;
+
+
+if ((sllary>=50000 && isGratuated === true && yearlyBonus >30000)|| perfermanceBonus == true && promotion == true ){
+ console.log('Yes I can Do this job in-sha-Allah')
+}
+else{
+    console.log("Sorry! No I can't do that job")
+}
